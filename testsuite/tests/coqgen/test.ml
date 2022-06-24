e9dd131c4db4360724381e0e93a82561647e1e02
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2022-06-24 17:24:01 +0900

    add Lazy.force

--- testsuite/tests/coqgen/test.ml
+++ testsuite/tests/coqgen/test.ml
@@ -63,6 +63,8 @@ let r = ref 1 in incr r;;
 
 (* lazy *)
 let lazy_counter c = lazy (incr c; !c);;
+let c = ref 0 in let m = lazy_counter c in let n = lazy_counter c in
+let n = Lazy.force n in let m = Lazy.force m in [m; n];;
 
 (* relaxed value restriction? *)
 let x = ref [] in !x;;
