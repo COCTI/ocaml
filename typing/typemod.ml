27a14fd4832bd297063bb75077b77575b82f91d9
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2022-09-02 09:30:55 +0900

    export Typecore.generalize_structure_exp

--- typing/typemod.ml
+++ typing/typemod.ml
@@ -2226,7 +2226,7 @@ and type_module_aux ~alias sttn funct_body anchor env smod =
   | Pmod_unpack sexp ->
       let exp =
         Ctype.wrap_def_principal (fun () -> Typecore.type_exp env sexp)
-          ~post:(fun exp -> Ctype.generalize_structure exp.exp_type)
+          ~post:Typecore.generalize_structure_exp
       in
       let mty =
         match get_desc (Ctype.expand_head env exp.exp_type) with
