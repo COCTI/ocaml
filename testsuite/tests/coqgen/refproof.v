f1c63e5f126ba59332c987cbf33bc058d7269738
maidaeito <maid.88@icloud.com>
2022-09-12 21:03:09 +0900

    write theorem

--- testsuite/tests/coqgen/refproof.v
+++ testsuite/tests/coqgen/refproof.v
@@ -1,21 +1,24 @@
 From mathcomp Require Import all_ssreflect.
 Require Import Sint63 BinNums ZArith cocti_defs test2.
+Check Z.of_nat.
+Check Z.to_nat.
+Check of_Z.
 
-Fixpoint fact_rec_pos (n : nat) : nat :=
+Fixpoint fact_rec_pos (n : nat) : int :=
   match n with
-  | 0 => 1
-  | m.+1 => m.+1 * fact_rec_pos m
+  | 0 => 1%sint63
+  | m.+1 => of_Z (Z.of_nat (m.+1)) * (fact_rec_pos m)
   end.
 
-Definition fact_rec (n : int) : nat := 
+Definition fact_rec (n : int) : int := 
   match to_Z n with
-  | Z0  => 1
+  | Z0  => 1%sint63
   | Zpos m => fact_rec_pos (Pos.to_nat m)
-  | Zneg _ => 1
+  | Zneg _ => 1%sint63
   end.
 
 Theorem fact_ok h n :
   inl_inv(snd(fact_for h n empty_env)) = fact_rec n.
 Proof.
-
+  
 Qed.
