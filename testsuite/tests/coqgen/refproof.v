b253946ccf69d65f74cdd274389239b588ff8b4f
maidaeito <maid.88@icloud.com>
2022-09-21 14:47:16 +0900

    add proof

--- testsuite/tests/coqgen/refproof.v
+++ testsuite/tests/coqgen/refproof.v
@@ -1,5 +1,5 @@
 From mathcomp Require Import all_ssreflect.
-Require Import Sint63 BinNums ZArith cocti_defs test2.
+Require Import Sint63 BinNums ZArith cocti_defs test2 sortproof.
 
 Axiom funext : forall A B (f g : A -> B), f =1 g -> f = g.
 
@@ -8,20 +8,20 @@ Proof. by move=> ->. Qed.
 
 Definition nat_to_int := fun n => of_Z (Z.of_nat n).
 Definition int_to_nat := fun n => Z.to_nat (to_Z n).
+Print Z.to_nat.
 
 Definition fact_rec_int n : int :=
-  match int_to_nat n with
-  | 0 => 1
-  | m.+1 => nat_to_int (m.+1 * fact_rec m)
-  end.
+  nat_to_int (fact_rec (int_to_nat n)).
+Print fact_rec.
 
-Lemma bindconst {A} {B} {m : M A} {env} {env'} {a} {e : M B} :
+Lemma bindinl {A} {B} {m : M A} {env} {env'} {a} {e : M B} :
   m env = (env', inl a) -> (m >> e) env = e env'.
 Proof. by rewrite /Bind => ->. Qed.
 
-Lemma fact_pure h n m env env' env'' : fact_for h n env = (Ret m) env' ->
+Lemma fact_closed h n m env env' env'' : fact_for h n env = (Ret m) env' ->
   (fact_for h n env).2 = (fact_for h n env'').2.
 Proof.
+  rewrite /fact_for /=.
   elim: h env env' => [ |h IH] env env'.
   -
   -
@@ -36,9 +36,43 @@ Proof.
   rewrite /fact_for.
 Qed.*)
 
-Theorem fact_ok h n m : fact_for h n = Ret m ->
+Definition at_loc {T} (l : loc T) (x : coq_type T) env :=
+  getref T l env = Ret x env.
+Locate "=1".
+Check eqfun.
+Locate eqfun.
+Check at_loc.
+
+Lemma forloop_cat h m n p b : (0 <=? m)%sint63 -> (m <=? n + 1)%sint63 ->
+  (n <=? p)%sint63 -> int_to_nat (p - m) <= h ->
+  forloop h m n b >>= (fun _ => forloop h n p b) = forloop h m p b.
+Proof. Admitted.
+
+Lemma int_to_natK n : (0 <=? n)%sint63 -> nat_to_int (int_to_nat n) = n.
+Admitted.
+
+Theorem fact_ok h n m env env' : fact_for h n env = Ret m env' ->
   m = fact_rec_int n.
 Proof.
+  rewrite /fact_for. rewrite {1}/Bind.
+  case H : (newref _ _ _) => [env'' [s|e]] //.
+  rewrite !bindretf.
+  case /boolP : (0 <=? n)%sint63.
+  move => Hn.
+  rewrite -(int_to_natK _ Hn).
+  elim : (int_to_nat n) m env' => [|{n Hn} n IH] m env'. admit.
+  rewrite -(forloop_cat h 2 (nat_to_int n)).
+  rewrite bindA.
+  rewrite {1}/Bind.
+  move : IH.
+  rewrite {1}/Bind.
+  case : (forloop h 2 (nat_to_int n) _ _) => [env''' [s'|e]].
+  rewrite
+  have -> : 2%sint63 = (k + 1)%sint63. admit.
+  have Hat : at_loc s (fact_rec_int k) env''. admit.
+  clear H.
+  
+
   case Hn : (to_Z n) => [|l|l].
   - have Hn0 : n = 0%sint63.
       by apply to_Z_inj.
