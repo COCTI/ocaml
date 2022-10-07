41ac7028ecc4a3024aabb6f84edff0b03fb6fd9a
Nakamura Kaoru <kaworunrun0804@gmail.com>
2022-10-07 16:03:11 +0900

    bindings

--- testsuite/tests/coqgen/refproof.v
+++ testsuite/tests/coqgen/refproof.v
@@ -1,5 +1,5 @@
 From mathcomp Require Import all_ssreflect.
-Require Import Uint63 BinNums ZArith cocti_defs test2 sortproof.
+Require Import Uint63 BinNums ZArith cocti_defs test2 sortproof Eqdep_dec.
 
 Axiom funext : forall A B (f g : A -> B), f =1 g -> f = g.
 
@@ -11,7 +11,6 @@ Definition int_to_nat := fun n => Z.to_nat (to_Z n).
 
 Definition fact_rec_int n : int :=
   nat_to_int (fact_rec (int_to_nat n)).
-Print fact_rec.
 
 Lemma bindinl {A} {B} {m : M A} {env} {env'} {a} {e : M B} :
   m env = (env', inl a) -> (m >> e) env = e env'.
@@ -41,29 +40,76 @@ Definition at_loc {T} (l : loc T) (x : coq_type T) env :=
 Lemma forloop_cat h m n p b : (0 <=? m)%uint63 -> (m <=? n + 1)%uint63 ->
   (n <=? p)%uint63 -> int_to_nat (p - m) <= h ->
   forloop h m n b >> forloop h (n + 1)%uint63 p b = forloop h m p b.
-Proof. Admitted.
+Proof.
+Admitted.
 
 Lemma int_to_natK n : nat_to_int (int_to_nat n) = n.
 Proof.
   rewrite /nat_to_int /int_to_nat.
-Search Z.of_nat Z.to_nat.
-  rewrite Z2Nat.id.
-  rewrite of_to_Z //.
+  rewrite Z2Nat.id ?of_to_Z //.
   by case : (to_Z_bounded n).
 Qed.
 
 Lemma nat_to_intK n : n < expn 2 63 -> int_to_nat (nat_to_int n) = n.
 Proof.
   rewrite /int_to_nat /nat_to_int.
-  Admitted.
+Admitted.
 
-Lemma newgetref_int x s env env' :
-  newref ml_int x env = (env', inl s) -> getref ml_int s env' = Ret x env'.
-Proof. Admitted.
+Definition RunM {A} (x : M A) env := RunW (x env).
 
-Lemma setgetref {T} x y s env env' :
-  setref T x y env = (env', inl s) -> getref T x env' = Ret y env'.
-Proof. Admitted.
+Lemma coerceE T x : coerce T T x = Some x.
+Proof.
+  rewrite /coerce.
+  case: ml_type_eq_dec => // a.
+  by rewrite -(eq_rect_eq_dec ml_type_eq_dec).
+Qed.
+
+Definition key_eqb (k1 k2 : key) :=
+  (key_id k1 =? key_id k2)%uint63 &&
+  (ml_type_eq_dec (key_type k1) (key_type k2)).
+
+Definition mem_bindings k :=
+  has (fun b => key_eqb k (bind_key M b)).
+
+Definition uniq_bindings env :=
+ forall c, count (fun b => key_id (bind_key M b) =? c)%uint63 env <= 1.
+
+Lemma lookup_updateE (k : key) (val : coq_type (key_type k)) env :
+  mem_bindings k env -> uniq_bindings env ->
+  obind (lookup k) (update (mkbind k val) env) = Some val.
+Proof.
+  elim: env => //= -[k' v env IH] /=.
+  rewrite /key_eqb.
+  case H: (key_id k =? key_id k')%uint63 => /=.
+  - case: ml_type_eq_dec => a //=.
+    + by rewrite Uint63.eqb_refl coerceE.
+    + admit.
+  - move=> Hmem Huniq.
+    have Huniq' : uniq_bindings env.
+      move=> x.
+      move: (Huniq x) => /=.
+      apply /leq_trans /leq_addl.
+    move: {IH} (IH Hmem Huniq').
+    case: update => //= env'.
+    by rewrite H.
+Admitted.
+
+Lemma newref_getE {T} x env :
+  RunM (do s <- newref T x; getref T s) env = inl x.
+Proof.
+  rewrite /RunM /newref /getref /Bind.
+  case: env => i l /=.
+  by rewrite Uint63.eqb_refl coerceE.
+Qed.
+
+Lemma setref_getE {T} x y env :
+  
+  RunM (do _ <- setref T x y; getref T x) env = inl y.
+Proof.
+  rewrite /RunM /setref /getref /Bind.
+  case: env => i l.
+  case: x y => k y /=.
+Admitted.
 
 Lemma nat_to_int_mul m n : (nat_to_int m * nat_to_int n)%uint63 =
   nat_to_int (m * n).
@@ -127,5 +173,4 @@ Proof.
     rewrite -lock leq_exp2l //.
     rewrite (ltn_trans (ltnSn _))//.
     rewrite (leq_trans Hn)// -lock leq_exp2l //.
-
 Admitted.
